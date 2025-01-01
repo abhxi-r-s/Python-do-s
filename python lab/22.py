18218955c6da88b86f35b0b8c528ac2f8ccfa12f
abhxi-r-s <abhishekunni8281@gmail.com>
2025-01-02 00:43:27 +0530

    22

---
+++ python lab/22.py
@@ -0,0 +1,9 @@
+for i in range(0,5):
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print("\n")
+
+for i in range(0,4):
+    for j in range(4,i-1,-1):
+        print("*",end=" ")
+    print("\n")
